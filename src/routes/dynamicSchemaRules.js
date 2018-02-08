b9a92787cbc08398b455747b39da84d6226e86de
JeremyCraigMartinez <jeremymartinez11@gmail.com>
2018-02-08 09:03:53 -0800

    use ajv as validator

--- src/routes/dynamicSchemaRules.js
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const inclusiveTimestamps = (time1, time2) => ((time1 < time2) ? 'timestamps are not inclusive' : null);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,22 @@
 import express from 'express';
-import { Validator } from 'jsonschema';
+import Ajv from 'ajv';
 
 import schemas from './schema.json';
 import sensor from './sensor';
 // import all other routes here
 
 const Router = express.Router();
-const v = new Validator();
+const ajv = new Ajv();
 
 Router.use((req, res, next) => {
   const schema = schemas[req.originalUrl][req.method];
-  v.addSchema(schema, schema.id);
-  const isValid = req.body && v.validate(req.body, schema.id);
+  const validate = ajv.compile(schema);
+  const isValid = req.body && validate(req.body);
+  console.log(validate.errors);
 
-  if (isValid.errors.length === 0) return next();
+  if (isValid === true) return next();
 
-  return next({ message: isValid.errors[0].message, statusCode: 400 });
+  return next({ message: validate.errors[0].message, statusCode: 400 });
 });
 
 Router.use('/sensor', sensor);
